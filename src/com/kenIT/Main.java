4936a9ed8c5997af0420444635d881ab89ac19df
- <=>
2022-04-15 17:03:15 +0700

    first commit

--- src/com/kenIT/Main.java
+++ src/com/kenIT/Main.java
@@ -3,6 +3,15 @@ package com.kenIT;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Count count = new Count();
+        try {
+            while (count.getMyThread().isAlive()) {
+                System.out.println("Main thread will be alive till the child thread is live");
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("\nMain thread run is over" );
     }
 }
